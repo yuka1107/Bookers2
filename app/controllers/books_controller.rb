bb9bdc93e0c2d06f12c7f5129f789776ffc30f4b
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-11-28 11:40:49 +0000

    [Add] ログインサインインのレイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
 
   def create
-    @book = Book.new(books_params)
+    @user = current_user
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to(book_path(@book.id))
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       @user = current_user
@@ -14,15 +15,15 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
     @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @newbook = Book.new
     @user = User.find(@book.user_id)
-    @user = @book.user
   end
 
   def edit
