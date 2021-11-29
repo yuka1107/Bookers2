980b37f166d7dbe1abad27914ff589aef89bb8c0
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-11-29 05:28:34 +0000

    [Add] テスト５回目エラー残り2つ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,7 +39,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.update(books_params)
       flash[:notice] = "You have updated book successfully."
-      redirect_to books_path(@book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
